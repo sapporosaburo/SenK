61eaabb8409b8baac80b06fe82288c59105292d6
KengoM1 <kengo.suzuki@eis.hokudai.ac.jp>
2022-05-20 00:16:13 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <random>
 
 #include "senk.hpp"
 #include "senk_test.hpp"
 
 int main(int argc, char **argv)
 {
-    std::string filename = "/Users/kengo/HPC/matrix/";
+    std::string filename = "/path_to/matrix/";
     filename += (argv[1]);
 
     double *tval;
@@ -21,56 +22,83 @@ int main(int argc, char **argv)
         &N, &M, &shape, removeZeros);
     //senk::matrix::RemoveZeros(&tval, &tcind, trptr, N);
     printf("# %d\n", trptr[N]);
-    //if(!senk::matrix::CheckStructure(tval, tcind, trptr, N)) {
-    //    printf("False: CheckStructure\n"); exit(1);
-    //}
+    if(!senk::matrix::CheckStructure<double>(tval, tcind, trptr, N)) {
+        printf("False: CheckStructure\n"); exit(1);
+    }
     double *val;
     int *cind;
     int *rptr;
     if(shape == senk::io::Shape::Sym) {
-        senk::matrix::Expand(tval, tcind, trptr, &val, &cind, &rptr, N);
+        senk::matrix::Expand<double>(
+            tval, tcind, trptr, &val, &cind, &rptr, N, "L");
     }else {
-        senk::matrix::Duplicate(tval, tcind, trptr, &val, &cind, &rptr, N);
+        senk::matrix::Duplicate<double>(
+            tval, tcind, trptr, &val, &cind, &rptr, N);
     }
     free(tval);
     free(tcind);
     free(trptr);
-    if(!senk::matrix::CheckStructure(val, cind, rptr, N)) exit(1);
 
-    printf("%d\n", rptr[N]);
+// Padding
     int ori_N = N;
-    const int bnl = 8;
-    const int bnw = 1;
-    senk::matrix::Padding(&val, &cind, &rptr, bnl, &N);
+    senk::matrix::Padding(&val, &cind, &rptr, 128, &N);
+    printf("%d\n", rptr[N]);
     M = N;
     printf("%d %d\n", N, M);
 
+// Reordering
+    int num_color;
+    int *size_color;
+    int *LP, *RP;
+    //senk::graph::GetAMCPermutation(
+    //    cind, rptr, &num_color, &size_color, &LP, &RP, N, false);
+    senk::graph::GetABMCPermutation(
+        cind, rptr, &num_color, &size_color, &LP, &RP, N, 128, false, "connect");
+    senk::matrix::Reordering<double>(val, cind, rptr, LP, RP, N);
+    printf("# ");
+    for(int i=0; i<num_color; i++) {
+        printf("%d ", size_color[i+1]-size_color[i]);   
+    }
+    printf("\n");
+    printf("# Reordered!\n");
+
+// Scaling
+    //double *tdiag;
+    //senk::matrix::GetDiag<double>(val, cind, rptr, &tdiag, N);
+    //double *ones = senk::utils::SafeMalloc<double>(N);
+    //for(int i=0; i<N; i++) { 
+    //    tdiag[i] = 1 / std::sqrt(std::abs(tdiag[i]));
+    //}
+    //senk::utils::Set<double>(1.0, ones, N);
+    //senk::matrix::Scaling<double>(val, cind, rptr, tdiag, tdiag, N);
+
+// ILU factorization
     senk::matrix::Duplicate(val, cind, rptr, &tval, &tcind, &trptr, N);
+    const int bnl = 8;
+    const int bnw = 1;
+    //senk::matrix::AllocBlockZero<double>(&tval, &tcind, &trptr, N, 2, 2);
+    //senk::matrix::AllocLevelZero<double>(&tval, &tcind, &trptr, N, 1);    
+    senk::matrix::AllocBlockZero<double>(&tval, &tcind, &trptr, N, bnl, bnw);
+    printf("# Allocated!\n");
 
-    double *bval;
-    int *bcind;
-    int *brptr;
-    senk::matrix::Csr2Bcsr<bnl, bnw>(tval, tcind, trptr, &bval, &bcind, &brptr, N);
-    free(tval);
-    free(tcind);
-    free(trptr);
-    senk::matrix::Bcsr2Csr<bnl, bnw>(bval, bcind, brptr, &tval, &tcind, &trptr, N);
+    //senk::matrix::AllocLevelZero(&tval, &tcind, &trptr, N, 2);
+    senk::matrix::Ilu0<double>(tval, tcind, trptr, N);
+    //senk::matrix::Ilup(&tval, &tcind, &trptr, N, 2);
+    printf("# Factrized!\n");
     
-    senk::matrix::Ilu0(tval, tcind, trptr, N);
-
-    //senk::matrix::Ilup(&tval, &tcind, &trptr, N, 5);
-
     double *lval;
     int *lcind;
     int *lrptr;
     double *uval;
     int *ucind;
     int *urptr;
-    senk::matrix::Split(
+    bool diagInv = true;
+    senk::matrix::Split<double>(
         tval, tcind, trptr,
         &lval, &lcind, &lrptr,
         &uval, &ucind, &urptr,
-        nullptr, N, "L-DU", true);
+        nullptr, N, "L-DU", diagInv);
+    printf("# Splited!\n");
 
     double *blval;
     int *blcind;
@@ -79,43 +107,45 @@ int main(int argc, char **argv)
     int *bucind;
     int *burptr;
 
-    senk::matrix::Csr2Bcsr<bnl, bnw>(lval, lcind, lrptr, &blval, &blcind, &blrptr, N);
-    senk::matrix::Csr2Bcsr<bnl, bnw>(uval, ucind, urptr, &buval, &bucind, &burptr, N);
+    senk::matrix::Csr2Bcsr<double>(lval, lcind, lrptr, &blval, &blcind, &blrptr, N, bnl, bnw);
+    senk::matrix::Csr2Bcsr<double>(uval, ucind, urptr, &buval, &bucind, &burptr, N, bnl, bnw);
 
     double *x = senk::utils::SafeMalloc<double>(N);
     double *b = senk::utils::SafeMalloc<double>(N);
-    for(int i=0; i<N; i++) {
-        x[i] = 0;
-        b[i] = 1;
-    }
+    senk::utils::Set<double>(0.0, x, N);
+    //senk::utils::Set<double>(1.0, b, N);
+    std::mt19937 engine(1);
+    std::uniform_real_distribution<> dist1(-1.0, 1.0);
+    for(int i=0; i<N; i++) { b[i] = dist1(engine); }
+    
+    for(int i=0; i<N-ori_N; i++) { b[ori_N+i] = 0.0; }
 
     int max_iter = 6000;
-    int gs_iter = 1;
-    int cycle_iter = 1;
     double epsilon = 1.0e-8;
     double nrm_b = senk::blas1::Nrm2(b, N);  
 
-    /*
-    senk::solver::MgGsCg(
-        lu_val, lu_cind, lu_rptr, diag,
-        lu_c_val, lu_c_cind, lu_c_rptr, c_diag,
-        i_val, i_cind, i_rptr,
-        r_val, r_cind, r_rptr,
-        b, x, nrm_b,
-        max_iter, N, dim, depth,
-        gs_iter, cycle_iter, epsilon, true);
-    */
+    auto start = std::chrono::system_clock::now();
 
-    senk::solver::Gmresm<double>(
+    //senk::solver::Gmresm<double>(
+    //    val, cind, rptr,
+    //    b, x, nrm_b, max_iter/50, 50, N, epsilon);
+    senk::solver::IluGmresm<double>(
         val, cind, rptr,
+        lval, lcind, lrptr, uval, ucind, urptr,
         b, x, nrm_b, max_iter/50, 50, N, epsilon);
-    //senk::solver::IluGmresm<double>(
+    //senk::solver::AbmcIluGmresm<double>(
     //    val, cind, rptr,
     //    lval, lcind, lrptr, uval, ucind, urptr,
+    //    size_color, num_color, 128,
     //    b, x, nrm_b, max_iter/50, 50, N, epsilon);
     //senk::solver::IlubGmresm<double, bnl, bnw>(
     //    val, cind, rptr,
     //    blval, blcind, blrptr, buval, bucind, burptr,
+    //    b, x, nrm_b, max_iter/50, 50, N, epsilon);
+    //senk::solver::AbmcIlubGmresm<double, bnl, bnw>(
+    //    val, cind, rptr,
+    //    blval, blcind, blrptr, buval, bucind, burptr,
+    //    size_color, num_color, 128,
     //    b, x, nrm_b, max_iter/50, 50, N, epsilon);
 
     //senk::solver::Bicgstab<double>(
@@ -132,164 +162,25 @@ int main(int argc, char **argv)
     
     //senk::solver::Gcrm<double>(
     //    val, cind, rptr,
-    //    b, x, nrm_b,
-    //    max_iter/50, 50, N, epsilon);
+    //    b, x, nrm_b, max_iter/50, 50, N, epsilon);
     //senk::solver::IluGcrm<double>(
     //    val, cind, rptr,
     //    lval, lcind, lrptr, uval, ucind, urptr,
-    //    b, x, nrm_b,
-    //    max_iter/50, 50, N, epsilon);
+    //    b, x, nrm_b, max_iter/50, 50, N, epsilon);
     //senk::solver::IlubGcrm<double, bnl, bnw>(
     //    val, cind, rptr,
     //    blval, blcind, blrptr, buval, bucind, burptr,
-    //    b, x, nrm_b,
-    //    max_iter/50, 50, N, epsilon);
+    //    b, x, nrm_b, max_iter/50, 50, N, epsilon);
 
-    /*
-    senk::solver::MgGsGcrm(
-        lu_val, lu_cind, lu_rptr, diag,
-        lu_c_val, lu_c_cind, lu_c_rptr, c_diag,
-        i_val, i_cind, i_rptr,
-        r_val, r_cind, r_rptr,
-        b, x, nrm_b,
-        max_iter/10, 10, N, dim, depth,
-        gs_iter, cycle_iter, epsilon, true);
-    */
-/*
-    double *csc_val;
-    int *csc_rind;
-    int *csc_cptr;
-    senk::matrix::Csr2Csc(val, cind, rptr, &csc_val, &csc_rind, &csc_cptr, N, N);
+    auto end = std::chrono::system_clock::now();
+    double elapsed 
+        = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
 
-    double *zt_val;
-    int *zt_cind;
-    int *zt_rptr;
-    double *wt_val;
-    int *wt_cind;
-    int *wt_rptr;
-    double *diag;
+    printf("%e\n", elapsed);
 
-    senk::ainv::left_painv(
-        val, cind, rptr,
-        csc_val, csc_rind, csc_cptr,
-        &zt_val, &zt_cind, &zt_rptr,
-        &wt_val, &wt_cind, &wt_rptr,
-        &diag, N, 0.1, 1.0);
-*/
-/*
-    senk::ainv::left_ainv(
-        val, cind, rptr,
-        csc_val, csc_rind, csc_cptr,
-        &zt_val, &zt_cind, &zt_rptr,
-        &wt_val, &wt_cind, &wt_rptr,
-        &diag, N, 0.1);
-*/
-/*
-    double *z_val;
-    int *z_cind;
-    int *z_rptr;
-    senk::matrix::Csr2Csc(zt_val, zt_cind, zt_rptr, &z_val, &z_cind, &z_rptr, N, N);
-    for(int i=0; i<N; i++) {
-        diag[i] = 1.0 / diag[i];
-    }
-
-    free(csc_val);
-    free(csc_rind);
-    free(csc_cptr);
-
-    double **zt_c_val = senk::utils::SafeMalloc<double*>(depth);
-    int **zt_c_cind   = senk::utils::SafeMalloc<int*>(depth);
-    int **zt_c_rptr   = senk::utils::SafeMalloc<int*>(depth);
-    double **wt_c_val = senk::utils::SafeMalloc<double*>(depth);
-    int **wt_c_cind   = senk::utils::SafeMalloc<int*>(depth);
-    int **wt_c_rptr   = senk::utils::SafeMalloc<int*>(depth);
-    double **c_diag   = senk::utils::SafeMalloc<double*>(depth);
-    double **z_c_val  = senk::utils::SafeMalloc<double*>(depth);
-    int **z_c_cind    = senk::utils::SafeMalloc<int*>(depth);
-    int **z_c_rptr    = senk::utils::SafeMalloc<int*>(depth);
-    for(int i=0; i<depth; i++) {
-        senk::matrix::Csr2Csc(c_val[i], c_cind[i], c_rptr[i], &csc_val, &csc_rind, &csc_cptr, dim[i], dim[i]);
-        senk::ainv::left_painv(
-            c_val[i], c_cind[i], c_rptr[i],
-            csc_val, csc_rind, csc_cptr,
-            &zt_c_val[i], &zt_c_cind[i], &zt_c_rptr[i],
-            &wt_c_val[i], &wt_c_cind[i], &wt_c_rptr[i],
-            &c_diag[i], dim[i], 0.1, 1.0);
-        //senk::ainv::left_ainv(
-        //    c_val[i], c_cind[i], c_rptr[i],
-        //    csc_val, csc_rind, csc_cptr,
-        //    &zt_c_val[i], &zt_c_cind[i], &zt_c_rptr[i],
-        //    &wt_c_val[i], &wt_c_cind[i], &wt_c_rptr[i],
-        //    &c_diag[i], dim[i], 0.05);
-        senk::matrix::Csr2Csc(
-            zt_c_val[i], zt_c_cind[i], zt_c_rptr[i],
-            &z_c_val[i], &z_c_cind[i], &z_c_rptr[i], dim[i], dim[i]);
-        for(int j=0; j<dim[i]; j++) { c_diag[i][j] = 1 / c_diag[i][j]; }
-        free(csc_val);
-        free(csc_rind);
-        free(csc_cptr);
-    }
-*/
-/*
-    float *fval = senk::utils::SafeMalloc<float>(rptr[N]);
-    float *fb = senk::utils::SafeMalloc<float>(N);
-    float *fx = senk::utils::SafeMalloc<float>(N);
-    senk::utils::Convert<double, float>(val, fval, rptr[N]);
-    senk::utils::Convert<double, float>(b, fb, N);
-    senk::utils::Convert<double, float>(x, fx, N);
-    float fnrm_b = senk::blas1::Nrm2(fb, N);
-    float fepsilon = 1.0e-8;
-    //senk::solver::Gmresm(
-    //    val, fval, cind, rptr,
-    //    b, x, nrm_b, 10, 20, N, epsilon);
-*/
-/*
-    senk::utils::Set<double>(0, x, N);
-    senk::solver::InvGmresm(
-        val, cind, rptr,
-        z_val, z_cind, z_rptr,
-        wt_val, wt_cind, wt_rptr, diag,
-        b, x, nrm_b, 100, 20, N, epsilon);
-*/
-/*
-    senk::solver::InvGmresm(
-        c_val[0], c_cind[0], c_rptr[0],
-        z_c_val[0], z_c_cind[0], z_c_rptr[0],
-        wt_c_val[0], wt_c_cind[0], wt_c_rptr[0], c_diag[0],
-        b, x, nrm_b, 100, 20, dim[0], epsilon);
-*/
-/*
-    senk::solver::MgInvGmresm(
-        val, cind, rptr,
-        wt_val, wt_cind, wt_rptr,
-        z_val, z_cind, z_rptr, diag,
-        c_val, c_cind, c_rptr,
-        wt_c_val, wt_c_cind, wt_c_rptr,
-        z_c_val, z_c_cind, z_c_rptr, c_diag,
-        i_val, i_cind, i_rptr,
-        r_val, r_cind, r_rptr,
-        b, x, nrm_b, 100, 20, N, dim, depth, epsilon);
-*/
-
-    //double *t = senk::utils::SafeMalloc<double>(N);
-    //double *t2 = senk::utils::SafeMalloc<double>(N);
-    //double *t3 = senk::utils::SafeMalloc<double>(N);
-/*
-    for(int i=0; i<10000; i++) {
-        senk::sparse::SpmvCsr(val, cind, rptr, x, t, N);
-        senk::blas1::Axpby(1, b, -1, t, N);
-        double nrm = senk::blas1::Nrm2(t, N);
-        printf("%e\n", nrm/nrm_b);
-        senk::solver::InverseSolve(
-            wt_val, wt_cind, wt_rptr, z_val, z_cind, z_rptr, diag,
-            t, t2, t3, N);
-        senk::blas1::Axpy(1, t2, x, N);
-    }
-*/
-    /*
     senk::test::RelativeResidualError(
         val, cind, rptr, b, x, N);
-    */
+    
     senk::utils::SafeFree<double>(&x);
 
     return 0;
